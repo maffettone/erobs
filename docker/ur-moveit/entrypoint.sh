36dc9aafc222dba0b95b021e06072b86da669233
maffettone <phillip.maffettone@gmail.com>
2023-08-14 15:44:46 +0000

    bld: docker containers for movit to include rviz

---
+++ docker/ur-moveit/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Starting VNC server..."
+# Setup VNC server
+Xvfb :1 -screen 0 2560x1440x16 &
+x11vnc -rfbport 5901 -bg -quiet -forever -shared -display :1
+
+
+echo "Starting rviz and moveit with ur_type ${UR_TYPE}..."
+.  /opt/ros/${ROS_DISTRO}/setup.bash
+ros2 launch ur_moveit_config ur_moveit.launch.py ur_type:=${UR_TYPE} launch_rviz:=${LAUNCH_RVIZ}
