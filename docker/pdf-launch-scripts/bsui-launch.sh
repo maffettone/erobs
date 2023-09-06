016fd95fd41cf563dffda816648aced600134cda
maffettone <phillip.maffettone@gmail.com>
2023-09-05 18:35:14 -0700

    ENH: Minimal working example of bsui in container at PDF

---
+++ docker/pdf-launch-scripts/bsui-launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+podman run -it --network host \
+    --env BS_ENV=$(basename "$BS_ENV") \
+    --env BS_PROFILE="collection" \
+    --env CDC_LOCALHOST=xf28id1-srv2.nsls2.bnl.local \
+    --env EPICS_CA_ADDR_LIST=10.66.219.255 \
+    --env EPICS_CA_AUTO_ADDR_LIST=no \
+    --mount type=bind,source=/nsls2/conda/envs,target=/opt/conda/envs,readonly \
+    --mount type=bind,source=/nsls2/data/pdf/shared/config/bluesky/profile_collection,target=/root/.ipython/profile_collection,readonly \
+    --mount type=bind,source=/nsls2/software/etc/tiled/profiles,target=/root/.config/tiled/profiles,readonly \
+    --mount type=bind,source=/etc/bluesky,target=/etc/bluesky,readonly \
+    ghcr.io/nsls2/erobs-bsui:latest
