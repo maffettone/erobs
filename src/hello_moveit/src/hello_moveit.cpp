6871bb47ef4e650d3714f77ccabe29b8eff95aaa
Chandima Fernando <wfernando1@bnl.gov>
2023-10-01 19:26:50 -0400

    add kinematric params

--- src/hello_moveit/src/hello_moveit.cpp
+++ src/hello_moveit/src/hello_moveit.cpp
@@ -38,7 +38,8 @@ int main(int argc, char * argv[])
   // Get robot config parameters from parameter server
   auto parameters = parent_parameters_client->get_parameters(
     {"robot_description_semantic",
-      "robot_description"});
+      "robot_description",
+      "robot_description_kinematics"});
 
   // create the Node for moveit with
   auto const node = std::make_shared<rclcpp::Node>(
@@ -50,6 +51,9 @@ int main(int argc, char * argv[])
   node->declare_parameter<std::string>("robot_description_semantic", parameter_value);
   parameter_value = parameters[1].value_to_string();
   node->declare_parameter<std::string>("robot_description", parameter_value);
+  parameter_value = parameters[2].value_to_string();
+  node->declare_parameter<std::string>("robot_description_kinematics", parameter_value);
+
 
   // Next step goes here
   // Create the MoveIt MoveGroup Interface
