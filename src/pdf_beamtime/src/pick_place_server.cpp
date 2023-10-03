8793e5f8a779aee91a9692e141a03da1e2e9e04d
Chandima Fernando <wfernando1@bnl.gov>
2023-10-03 15:34:41 +0000

    remove unused files + clean code

--- src/pdf_beamtime/src/pick_place_server.cpp
+++ src/pdf_beamtime/src/pick_place_server.cpp
@@ -21,17 +21,8 @@ BSD 3 Clause License. See LICENSE.txt for details.*/
 
 using namespace std::chrono_literals;
 
-/*
-This Action server cannot use the plugin approaches, because it is not a node with standard initialization.
-Instead of following the tutorials here directl we need to spin a node in a main function.
-https://docs.ros.org/en/humble/Tutorials/Intermediate/Writing-an-Action-Server-Client/Cpp.html
+// This only adds the obstacle environment
 
-I.e. using a namespace and RCLCPP_COMPONENTS_REGISTER_NODE(action_tutorials_cpp::FibonacciActionServer)
-and rclcpp_components_register_nodes will not work. It will create an compilation error:
-/usr/include/c++/11/ext/new_allocator.h:162:11: \
-error: no matching function for call to ‘pick_place_repeat_server::PickPlaceServer::PickPlaceServer(const rclcpp::NodeOptions&)’
-{ ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }
-*/
 class PickPlaceServer
 {
 public:
@@ -45,28 +36,10 @@ public:
     using namespace std::placeholders;
     using namespace std::chrono_literals;
 
-    // Create parameters for list of waypoints
-    RCLCPP_INFO(node_->get_logger(), "Not declaring parameters for waypoints");
-
-    RCLCPP_INFO(node_->get_logger(), "Reading parameters for waypoints");
-    home_pose_ = node_->get_parameter("home_pose").as_double_array();
-
-    // Read in travel waypoints as Poses from config/waypoints.yaml file
-    auto const waypoints_file = node_->get_parameter("waypoints_file").as_string();
-    travel_waypoints = read_waypoints(waypoints_file);
-
     // Add the obstacles
     planning_scene_interface = new moveit::planning_interface::PlanningSceneInterface();
     planning_scene_interface->applyCollisionObjects(create_env());
 
-
-    action_server_ = rclcpp_action::create_server<PickPlaceRepeat>(
-      node_,
-      "pick_place_repeat",
-      std::bind(&PickPlaceServer::handle_goal, this, _1, _2),
-      std::bind(&PickPlaceServer::handle_cancel, this, _1),
-      std::bind(&PickPlaceServer::handle_accepted, this, _1));
-    RCLCPP_INFO(node_->get_logger(), "PickPlaceServer has been initialized.");
   }
 
   rclcpp::node_interfaces::NodeBaseInterface::SharedPtr getNodeBaseInterface()
@@ -78,154 +51,10 @@ public:
 private:
   rclcpp::Node::SharedPtr node_;
   moveit::planning_interface::MoveGroupInterface move_group_interface_;
-  rclcpp_action::Server<PickPlaceRepeat>::SharedPtr action_server_;
-  int num_repeats_;
-  int total_steps_ = 2 * 8;
-  std::vector<geometry_msgs::msg::Pose> travel_waypoints;
-  std::vector<double> home_pose_;
 
   moveit::planning_interface::PlanningSceneInterface * planning_scene_interface;
   std::map<std::string, int> obstacle_type_map;
 
-  std::vector<geometry_msgs::msg::Pose> read_waypoints(const std::string & filename)
-  {
-    /* Function to read in a series of poses from a yaml file*/
-    std::vector<geometry_msgs::msg::Pose> waypoints;
-    RCLCPP_INFO(node_->get_logger(), "Reading waypoints from %s", filename.c_str());
-
-    std::ifstream file(filename);
-    YAML::Node yaml_node = YAML::Load(file);
-    for (auto const & pose : yaml_node) {
-      geometry_msgs::msg::Pose msg;
-      const YAML::Node & poseNode = pose["pose"];
-      msg.position.x = poseNode["position"]["x"].as<double>();
-      msg.position.y = poseNode["position"]["y"].as<double>();
-      msg.position.z = poseNode["position"]["z"].as<double>();
-      msg.orientation.x = poseNode["orientation"]["x"].as<double>();
-      msg.orientation.y = poseNode["orientation"]["y"].as<double>();
-      msg.orientation.z = poseNode["orientation"]["z"].as<double>();
-      msg.orientation.w = poseNode["orientation"]["w"].as<double>();
-      waypoints.push_back(msg);
-    }
-    return waypoints;
-  }
-
-  // Receiving the goal request. Simply asking for, do the thing several times.
-  rclcpp_action::GoalResponse handle_goal(
-    const rclcpp_action::GoalUUID & uuid,
-    std::shared_ptr<const PickPlaceRepeat::Goal> goal)
-  {
-    RCLCPP_INFO(node_->get_logger(), "Received goal request with order %d", goal->repeats);
-    (void)uuid;
-    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
-  }
-
-  // Receiving the cancel request.
-  rclcpp_action::CancelResponse handle_cancel(
-    const std::shared_ptr<rclcpp_action::ServerGoalHandle<PickPlaceRepeat>> goal_handle)
-  {
-    RCLCPP_INFO(node_->get_logger(), "Received request to cancel goal");
-    (void)goal_handle;
-    return rclcpp_action::CancelResponse::ACCEPT;
-  }
-
-  // Spawn a thread to actually do the work.
-  void handle_accepted(
-    const std::shared_ptr<rclcpp_action::ServerGoalHandle<PickPlaceRepeat>> goal_handle)
-  {
-    using namespace std::placeholders;
-
-    // Start the thread to execute the action
-    std::thread{std::bind(&PickPlaceServer::execute, this, _1), goal_handle}.detach();
-  }
-
-  // Helper function to plan and execute in joint space
-  bool plan_and_execute_joint_target(const std::vector<double> & angles)
-  // TODO(maffettone): take the goal handle and add cancellation infrastructure
-  {
-    move_group_interface_.setJointValueTarget(angles);
-    // Create a plan to that target pose
-    auto const [success, plan] = [this] {
-        moveit::planning_interface::MoveGroupInterface::Plan msg;
-        auto const ok = static_cast<bool>(this->move_group_interface_.plan(msg));
-        return std::make_pair(ok, msg);
-      }();
-    // Execute the plan
-    if (success) {
-      move_group_interface_.execute(plan);
-    } else {
-      RCLCPP_ERROR(node_->get_logger(), "Planning failed!");
-    }
-    return success;
-  }
-
-
-  // Helper function to plan and execute based on pose
-  bool plan_and_execute_pose(const geometry_msgs::msg::Pose & pose)
-  // TODO(maffettone): take the goal handle and add cancellation infrastructure
-  {
-    move_group_interface_.setPoseTarget(pose);
-    // Create a plan to that target pose
-    auto const [success, plan] = [this] {
-        moveit::planning_interface::MoveGroupInterface::Plan msg;
-        auto const ok = static_cast<bool>(this->move_group_interface_.plan(msg));
-        return std::make_pair(ok, msg);
-      }();
-    // Execute the plan
-    if (success) {
-      move_group_interface_.execute(plan);
-    } else {
-      RCLCPP_ERROR(node_->get_logger(), "Planning failed!");
-    }
-    return success;
-  }
-
-  // The actual work. Perform the movement between waypoints several times, tracking progress.
-  void execute(const std::shared_ptr<rclcpp_action::ServerGoalHandle<PickPlaceRepeat>> goal_handle)
-  {
-    RCLCPP_INFO(node_->get_logger(), "Executing goal");
-
-    // Helper variables
-    const auto goal = goal_handle->get_goal();
-    this->num_repeats_ = goal->repeats;
-    auto feedback = std::make_shared<PickPlaceRepeat::Feedback>();
-    auto & num_completed = feedback->num_completed;
-    auto & percent_completed = feedback->percent_completed;
-    auto & percentage_current = feedback->percentage_current;
-    auto result = std::make_shared<PickPlaceRepeat::Result>();
-    bool success = true;
-    auto loop_rate = rclcpp::Rate(3s);
-
-
-    // Perform the movement between waypoints several times, tracking progress.
-    for (num_completed = 0; num_completed < num_repeats_; ++num_completed) {
-      percent_completed = static_cast<float>(num_completed) / num_repeats_;
-      percentage_current = 0.0;
-      goal_handle->publish_feedback(feedback);
-      RCLCPP_INFO(node_->get_logger(), "Completed %d of %d repeats", num_completed, num_repeats_);
-      // Move to home pose
-      RCLCPP_INFO(node_->get_logger(), "Moving to home pose in joint space");
-
-      success = plan_and_execute_joint_target(home_pose_);
-      if (!success) {
-        result->success = false;
-        goal_handle->succeed(result);
-        return;
-      }
-      percentage_current += 1.0 / total_steps_;
-      goal_handle->publish_feedback(feedback);
-
-      // Sleep for a while
-      loop_rate.sleep();
-    }
-
-    // Send the result
-    if (rclcpp::ok()) {
-      result->success = true;
-      goal_handle->succeed(result);
-      RCLCPP_INFO(node_->get_logger(), "Goal succeeded");
-    }
-  }
 
   std::vector<moveit_msgs::msg::CollisionObject> create_env()
   {
