c6f3a42f91472e9b602066aabe8cdb3181c095cd
Chandima Fernando <wfernando1@bnl.gov>
2023-10-03 12:46:35 -0400

    remove entrypoint from hello-world

--- docker/hello-world/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Starting custom program ... "
-. /opt/ros/${ROS_DISTRO}/setup.bash
-. install/setup.bash 
-# ros2 run hello_moveit hello_moveit
-ro2 launch hello_moveit pick_place_repeat.launch.py
