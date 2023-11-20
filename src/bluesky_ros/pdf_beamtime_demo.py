d16ff71ee6427355ba9a8a169eb544df607874c4
Chandima Fernando <wfernando1@bnl.gov>
2023-11-20 10:04:41 -0500

    fix: fix lint errors

--- src/bluesky_ros/pdf_beamtime_demo.py
+++ src/bluesky_ros/pdf_beamtime_demo.py
@@ -1,8 +1,8 @@
 import bluesky.plan_stubs as bps
 import math
 import rclpy
+
 from bluesky import RunEngine
-from ophyd.sim import det1, motor1
 from rclpy.task import Future
 
 from bluesky_ros.ophyd_ros import ActionMovable
@@ -10,38 +10,44 @@ from pdf_beamtime_interfaces.action import PickPlaceControlMsg
 
 
 class PickPlaceRepeatDevice(ActionMovable):
+    """Construct a class to build a new device object."""
+
     action_type = PickPlaceControlMsg
     parent = None
 
     def construct_goal_mesage(self, *args, **kwargs):
+        """Populate the goal msg."""
         goal_msg = PickPlaceControlMsg.Goal()
-        goal_msg.pickup_approach = kwargs.get("pickup_approach")
-        goal_msg.pickup = kwargs.get("pickup")
+        goal_msg.pickup_approach = kwargs.get('pickup_approach')
+        goal_msg.pickup = kwargs.get('pickup')
         goal_msg.place_approach = kwargs.get("place_approach")
         goal_msg.place = kwargs.get("place")
         return goal_msg
 
     def feedback_callback(self, feedback_msg):
+        """Handle regular feedback and print the completion percentage."""
         feedback = feedback_msg.feedback
         self.get_logger().info("Completion percentage: {0} %".format(math.ceil(feedback.status * 100)))
 
     def get_result_callback(self, future: Future):
+        """Handle results at the end of a state transition."""
         return super().get_result_callback(future)
 
 
 def plan(node, joint_goals):
+    """Planning sequence goes here."""
     yield from bps.mv(node, joint_goals)
 
 
 rclpy.init()
 
-node = PickPlaceRepeatDevice(node_name="pdf_beamtime_control_device", action_client_name="pdf_beamtime_action_server")
+node = PickPlaceRepeatDevice(node_name='pdf_beamtime_control_device', action_client_name='pdf_beamtime_action_server')
 
 joint_goals = {
-    "pickup_approach": [1.466, -2.042, -2.1293, -2.164, -0.105, 0.0],
-    "pickup": [1.099, -2.234, -1.728, -2.339, -0.489, -0.035],
-    "place_approach": [2.618, -2.356, -1.763, -2.216, -2.215, 0.0],
-    "place": [2.618, -2.356, -1.663, -2.416, -2.215, 0.0],
+    'pickup_approach': [1.466, -2.042, -2.1293, -2.164, -0.105, 0.0],
+    'pickup': [1.099, -2.234, -1.728, -2.339, -0.489, -0.035],
+    'place_approach': [2.618, -2.356, -1.763, -2.216, -2.215, 0.0],
+    'place': [2.618, -2.356, -1.663, -2.416, -2.215, 0.0],
 }
 
 RE = RunEngine({})
--- src/bluesky_ros/re_demo.py
+++ src/bluesky_ros/re_demo.py
@@ -9,15 +9,18 @@ from hello_moveit_interfaces.action import PickPlaceRepeat
 
 
 class PickPlaceRepeatDevice(ActionMovable):
+    """Construct a class to build a new device object."""
     action_type = PickPlaceRepeat
     parent = None
 
     def construct_goal_mesage(self, *args, **kwargs):
+        """Populate the goal msg."""
         goal_msg = PickPlaceRepeat.Goal()
         goal_msg.repeats = args[0]
         return goal_msg
 
     def feedback_callback(self, feedback_msg):
+        """Handle regular feedback and print the completion percentage."""
         feedback: PickPlaceRepeat.Feedback = feedback_msg.feedback
         self.get_logger().info(
             f"Number completed: {feedback.num_completed}. "
@@ -26,10 +29,12 @@ class PickPlaceRepeatDevice(ActionMovable):
         )
 
     def get_result_callback(self, future: Future):
+        """Handle results at the end of a state transition."""
         return super().get_result_callback(future)
 
 
 def plan(node, det, motor):
+    """Planning sequence goes here."""
     yield from bps.mv(motor, 0)
     yield from bps.mv(node, 1)
     yield from bps.read(det)
@@ -37,7 +42,7 @@ def plan(node, det, motor):
 
 rclpy.init()
 
-node = PickPlaceRepeatDevice(node_name="pick_place_repeat_device", action_client_name="pick_place_repeat")
+node = PickPlaceRepeatDevice(node_name='pick_place_repeat_device', action_client_name='pick_place_repeat')
 
 RE = RunEngine({})
 RE(plan(node, det1, motor1))
