34a6922abc00400ea25b0d72e719c5b9891a36e0
Chandima Fernando <wfernando1@bnl.gov>
2023-11-29 03:05:11 -0500

    fix: separate hande from org

--- docker/ur-driver/entrypoint.sh
+++ docker/ur-driver/entrypoint.sh
@@ -4,4 +4,4 @@ echo "Connecting to simulator through ursim for ur_type ${UR_TYPE}..."
 .  /opt/ros/${ROS_DISTRO}/setup.bash
 .  /root/ws/install/setup.bash 
 
-ros2 launch ur_robot_driver ur_control.launch.py ur_type:=${UR_TYPE} robot_ip:=${ROBOT_IP} launch_rviz:=false description_package:=${DESCRIPTION_PKG}
+ros2 launch ur_robot_driver ur_control.launch.py ur_type:=${UR_TYPE} robot_ip:=${ROBOT_IP} launch_rviz:=false description_package:=${DESCRIPTION_PKG} description_file:="hande_extension.urdf.xacro"
