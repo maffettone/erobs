53177594f77645ce99c56ab76fbd1a4e3ed061b3
Chandima Fernando <wfernando1@bnl.gov>
2024-03-28 00:59:07 -0400

    fix: more path corrections

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ set -e
 vcs import < /root/ws/src/erobs/src/ros2.repos /root/ws/src/erobs/src
 sudo apt-get update
 rosdep update
-rosdep install --from-paths src --ignore-src -y
+rosdep install --from-paths /root/ws/src --ignore-src -y
