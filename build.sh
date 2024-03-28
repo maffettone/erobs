295887a33101a729c669b3f7cd70c516c9293c76
Chandima Fernando <wfernando1@bnl.gov>
2024-03-27 23:57:39 -0400

    fix: cmake args temp fix

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ BUILD_TYPE=RelWithDebInfo
 source /opt/ros/${ROS_DISTRO}/setup.bash && colcon build \
         --merge-install \
         --symlink-install \
-        --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
         --packages-select ur3e_hande_robot_description serial robotiq_driver pdf_beamtime_interfaces \
-        -Wall -Wextra -Wpedantic
+        --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
+        --cmake-args "-DCMAKE_CXX_FLAGS=-Wall -Wextra -Wpedantic"
 
 colcon build \
         --merge-install \
