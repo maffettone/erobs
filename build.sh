a79af471ee5ef3f7ce0bcc0e10980890f5e26eec
Chandima Fernando <wfernando1@bnl.gov>
2024-03-27 21:44:28 -0400

    fix: point to build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,14 @@ set -e
 
 # Set the default build type
 BUILD_TYPE=RelWithDebInfo
+
+source /opt/ros/${ROS_DISTRO}/setup.bash && colcon build \
+        --merge-install \
+        --symlink-install \
+        --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
+        --packages-select ur3e_hande_robot_description serial robotiq_driver pdf_beamtime_interfaces \
+        -Wall -Wextra -Wpedantic
+
 colcon build \
         --merge-install \
         --symlink-install \
