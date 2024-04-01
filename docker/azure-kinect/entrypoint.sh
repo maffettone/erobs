2b80a26e552d76a7e3dc46b7ee3d0b5c506cca6e
Chandima Fernando <wfernando1@bnl.gov>
2024-04-01 15:52:22 -0400

    feat: add azure ros2 node

--- docker/azure-kinect/entrypoint.sh
+++ docker/azure-kinect/entrypoint.sh
@@ -5,10 +5,9 @@ echo "Starting VNC server..."
 Xvfb :1 -screen 0 2560x1440x16 &
 x11vnc -rfbport 5905 -bg -quiet -forever -shared -display :1
 
-# sudo k4aviewer
+echo "source the workspace"
+.  /opt/ros/${ROS_DISTRO}/setup.bash
+. /root/ws/install/setup.bash
 
-# echo "Starting rviz and moveit with ur_type ${UR_TYPE}..."
-# .  /opt/ros/${ROS_DISTRO}/setup.bash
-# . /root/ws/install/setup.bash
-
-# ros2 launch ur_moveit_config ur_moveit.launch.py ur_type:=${UR_TYPE} launch_rviz:=${LAUNCH_RVIZ} description_package:=${DESCRIPTION_PKG}  launch_servo:=false description_file:=${DESCRIPTION_FILE} moveit_config_package:=${CONFIG_PKG} moveit_config_file:=${CONFIG_FILE}
+echo "run driver.launch"
+ros2 launch azure_kinect_ros_driver driver.launch.py
