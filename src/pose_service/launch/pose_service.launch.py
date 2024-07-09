ba3d26b10a16c4805c55a852f94ab1560572e51b
Chandima Fernando <wfernando1@bnl.gov>
2024-07-09 15:02:07 -0400

    fix: lint error ros

--- src/pose_service/launch/pose_service.launch.py
+++ src/pose_service/launch/pose_service.launch.py
@@ -6,15 +6,17 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     """Launch the node pose_service with parameter files."""
-    return LaunchDescription([
-        Node(
-            package="pose_service",
-            executable="pose_service",
-            name="pose_service",
-            output="screen",
-            parameters=[
-                 PathJoinSubstitution([FindPackageShare("pose_service"), "config", "camera_param.yaml"]),
-                 PathJoinSubstitution([FindPackageShare("pose_service"), "config", "fiducial_marker_param.yaml"])
-            ]
-        )
-    ])
+    return LaunchDescription(
+        [
+            Node(
+                package="pose_service",
+                executable="pose_service",
+                name="pose_service",
+                output="screen",
+                parameters=[
+                    PathJoinSubstitution([FindPackageShare("pose_service"), "config", "camera_param.yaml"]),
+                    PathJoinSubstitution([FindPackageShare("pose_service"), "config", "fiducial_marker_param.yaml"]),
+                ],
+            )
+        ]
+    )
