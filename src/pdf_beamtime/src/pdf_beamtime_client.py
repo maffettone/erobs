68e655e17fb3fa632f8d3bd7cf787de6aee889f8
Chandima Fernando <wfernando1@bnl.gov>
2024-08-07 14:05:52 +0000

    feat: add joint return coords

--- src/pdf_beamtime/src/pdf_beamtime_client.py
+++ src/pdf_beamtime/src/pdf_beamtime_client.py
@@ -19,20 +19,20 @@ class SimpleClient(Node):
         self._action_client = ActionClient(self, PickPlaceControlMsg, "pdf_beamtime_action_server")
         self._goal_handle = None
 
-    def send_goal(self):
+    def send_pickup_goal(self):
         """Send a working goal."""
         goal_msg = PickPlaceControlMsg.Goal()
 
         goal_msg.pickup_approach = [240.86, -63.96, 127.57, -61.50, 98.26, 180.0]
         goal_msg.pickup_approach = [x / 180 * math.pi for x in goal_msg.pickup_approach]
 
-        goal_msg.pickup = [236.97, -54.79, 108.08, -51.21, 94.39, 180.0]
+        goal_msg.pickup = [236.54, -52.75, 106.64, -51.82, 93.97, 180.0]
         goal_msg.pickup = [x / 180 * math.pi for x in goal_msg.pickup]
 
         goal_msg.place_approach = [54.55, -84.79, 120.92, -36.14, 51.53, 180.0]
         goal_msg.place_approach = [x / 180 * math.pi for x in goal_msg.place_approach]
 
-        goal_msg.place = [62.16, -70.19, 100.56, -30.42, 59.15, 180.0]
+        goal_msg.place = [62.15, -71.66, 98.75, -27.14, 59.13, 180.0]
         goal_msg.place = [x / 180 * math.pi for x in goal_msg.place]
 
         self._action_client.wait_for_server()
@@ -50,13 +50,33 @@ class SimpleClient(Node):
         self.get_logger().warn("********** Goal Canceling Now *********")
         self._goal_handle.cancel_goal_async()
 
+    def send_return_sample_goal(self):
+        """Send a working goal."""
+        goal_msg = PickPlaceControlMsg.Goal()
+
+        goal_msg.pickup_approach = [54.55, -84.79, 120.92, -36.14, 51.53, 180.0]
+        goal_msg.pickup_approach = [x / 180 * math.pi for x in goal_msg.pickup_approach]
+
+        goal_msg.pickup = [62.15, -71.66, 98.75, -27.14, 59.13, 180.0]
+        goal_msg.pickup = [x / 180 * math.pi for x in goal_msg.pickup]
+
+        goal_msg.place_approach = [240.86, -63.96, 127.57, -61.50, 98.26, 180.0]
+        goal_msg.place_approach = [x / 180 * math.pi for x in goal_msg.place_approach]
+
+        goal_msg.place = [236.54, -52.75, 106.64, -51.82, 93.97, 180.0]
+        goal_msg.place = [x / 180 * math.pi for x in goal_msg.place]
+
+        self._action_client.wait_for_server()
+        self._send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
+
 
 def main(args=None):
     """Python main."""
     rclpy.init(args=args)
 
     client = SimpleClient()
-    client.send_goal()
+    # client.send_pickup_goal()
+    client.send_return_sample_goal()
 
     rclpy.spin(client)
 
